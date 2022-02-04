04ff65a17ac7d9987d6cc911768ae381d50b7e14
jeelpatel231 <jeelpatel231@gmail.com>
2022-02-04 23:21:32 +0530

    fix gradlew ootb permission and node install deps

--- build.sh
+++ build.sh
@@ -1,12 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+if [[ $(command -v yarn) ]]; then 
+    pkgman=yarn;
+elif [[ $(command -v npm) ]]; then 
+    pkgman=npm;
+else 
+    echo -e "no node package managers found,\ninstall yarn or npm to continue\n";
+    exit 1;
+fi
 
 cd web
-npm run build # build vue project
+$pkgman install # install deps
+$pkgman run build # build vue project
 rm -rf ../cors/dist # remove existing dir in cors folder, if exists
 cp -r dist ../cors/dist # copy production build in cors-nginx for serving static files
 
 
 cd ../api
+chmod +x gradlew
 ./gradlew shadowjar # build api FAT jar for running in java docker
 
 docker-compose build
